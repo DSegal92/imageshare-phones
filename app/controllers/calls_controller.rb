e2873061d54282b64c489f62ad50983f4fc6b708
Daniel Segal <DSegal92@gmail.com>
2012-07-13 15:52:50 -0400

    TIME IS WRONG

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -12,7 +12,7 @@ class CallsController < ApplicationController
   		newcall.caller_ID = params[:callerID]
   		@end_time = params[:ended]
   		@start_time = params[:started]
-  		@elapsed = @end_time.to_f - @start_time.to_f
+  		@elapsed = @end_time.to_f
   		newcall.length = @elapsed.to_s
   		newcall.save  		
   end
