50893740d49903d6c9189ad72270a51436123817
Daniel Segal <DSegal92@gmail.com>
2012-07-16 10:23:50 -0400

    testing

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -12,8 +12,10 @@ class CallsController < ApplicationController
   		newcall.target= params[:destination]
   		newcall.origin = params[:origin]
   		newcall.caller_ID = params[:callerID]
+      newcall.was_connected = params[:connected]
   		@end_time = params[:ended]
   		@start_time = params[:started]
+
   		@elapsed = @end_time.to_f
   		newcall.length = @elapsed.to_s
   		newcall.save  		
