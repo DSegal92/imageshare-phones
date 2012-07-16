36b7a2c1f6bd88134d11af1c94967f4085b5055b
Daniel Segal <DSegal92@gmail.com>
2012-07-16 11:27:32 -0400

    Testing

--- app/admin/calls.rb
+++ app/admin/calls.rb
@@ -1,3 +1,17 @@
 ActiveAdmin.register Call do
-  
+  #attr_accessible :answered_id, :caller_ID, :length, :location, :notes, :origin, :target, :times_called, :was_connected
+  index do
+  	column :id
+  	column :target
+  	column :origin
+  	column :length
+  	column :times_called
+  	column :was_connected
+  	column :caller_ID
+  	column :location
+  	column :notes
+  	column :created_at
+  	column :updated_at
+  	default_actions
+  end
 end
