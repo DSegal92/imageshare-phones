bdd386b688a09d6feacacaaacca2be9359d3d218
Daniel Segal <DSegal92@gmail.com>
2012-07-19 15:42:24 -0400

    converts seconds to time

--- app/admin/calls.rb
+++ app/admin/calls.rb
@@ -13,7 +13,7 @@ ActiveAdmin.register Call do
         link_to call.answered, @searchURL
       end
     end 
-  	column "Caller ID", :sortable => :caller_ID do |call|
+    column "Caller ID", :sortable => :caller_ID do |call|
       unless call.caller_ID.nil?
         @searchURL = '/admin/calls?&q%5Bcaller_ID_contains%5D=' + call.caller_ID
         link_to call.caller_ID, @searchURL
@@ -24,36 +24,34 @@ ActiveAdmin.register Call do
         Call.find_all_by_caller_ID(call.caller_ID).size
       end
     end
-  	column "Call Length (s)" do |call|
-      unless call.menuTime.nil?
-  		  call.menuTime.to_i / 60 + " min " + call.menuTime.to_i % 60 + " s"
-      end
-  	end 
-  	column "Site", :sortable => :site do |call|
+    column "Call Length" do |call|
+      call.getTime(call.menuTime)
+    end 
+    column "Site", :sortable => :site do |call|
       unless call.site.nil?
         @searchURL = '/admin/calls?&q%5Blocation_contains%5D=' + call.site
         link_to call.site, @searchURL
       end
     end
     column :location
-  	column "Notes" do |call|
+    column "Notes" do |call|
       unless call.notes.nil?
         call.notes.truncate(10)
       end
     end
     column :was_connected
     column :session   
-  	column :created_at
-  	column :updated_at
-  	default_actions
+    column :created_at
+    column :updated_at
+    default_actions
   end
 
- form do |f|
+  form do |f|
    f.inputs do
      f.input :target
      f.input :answered, :label => "Answered By"
      f.input :caller_ID, :label => "Caller ID"
-     f.input :menuTime, :label => "Time Spent in Menu (s)"
+     f.input :menuTime, :label => "Call Length (s)"
      f.input :site, :as => :select, :collection => ["A", "B", "C", "D", "E"]
      f.input :location 
      f.input :notes
@@ -63,41 +61,41 @@ ActiveAdmin.register Call do
    f.buttons
  end
 
-  show do |ad|
-    attributes_table do
-      table do
-        tr
-          th 'Target'
-          td call.target
-          th 'Answered By'
-          td call.answered
-        tr
-          th 'Caller ID'
-          td call.caller_ID
-          th 'Length In Menu (s)'
-          td call.menuTime
-        tr
-          th 'Site'
-          td call.site
-          th 'Location'
-          td call.location
-        tr
-          th 'Times Called'
-          td Call.find_all_by_caller_ID(call.caller_ID).size
-          th 'Was Connected?'
-          if call.was_connected?
-            td "True"
-          else
-            td "False"
-          end
+ show do |ad|
+  attributes_table do
+    table do
+      tr
+      th 'Target'
+      td call.target
+      th 'Answered By'
+      td call.answered
+      tr
+      th 'Caller ID'
+      td call.caller_ID
+      th 'Call Length'
+      td call.getTime(call.menuTime)
+      tr
+      th 'Site'
+      td call.site
+      th 'Location'
+      td call.location
+      tr
+      th 'Times Called'
+      td Call.find_all_by_caller_ID(call.caller_ID).size
+      th 'Was Connected?'
+      if call.was_connected?
+        td "True"
+      else
+        td "False"
+      end
 
-      end  
-      table do
-        tr
-          th 'Notes'
-          td call.notes
-      end        
-    end
-  end 
+    end  
+    table do
+      tr
+      th 'Notes'
+      td call.notes
+    end        
+  end
+end 
 
 end
--- app/models/call.rb
+++ app/models/call.rb
@@ -1,4 +1,23 @@
 class Call < ActiveRecord::Base
   attr_accessible :answered, :caller_ID, :location, :notes, :origin, :target, :was_connected, :menuTime, :timesCalled, :site
-
+  def getTime(time)
+  	time = time.to_i
+	hours = (time / 3600)
+		hourPrint = hours.to_s
+		if hours < 10
+			hourPrint = '0' + hours.to_s
+		end
+	minutes = (time / 60) - (hours * 60)
+		minutePrint = minutes.to_s
+		if minutes < 10
+				minutePrint = '0' + minutes.to_s
+			end
+	seconds = time % 60
+		secondPrint = seconds.to_s
+		if seconds < 10
+				secondPrint = '0' + seconds.to_s
+			end
+	hourPrint + ':' + minutePrint + ':' + secondPrint
+	
+  end
 end
