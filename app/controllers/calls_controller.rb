605449578df92d2f09e1f509d2fc5f5066636555
Daniel Segal <DSegal92@gmail.com>
2012-07-19 14:00:36 -0400

    testing

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -49,9 +49,14 @@ class CallsController < ApplicationController
   end
 
   def startCall
-    newcall = Call.create
+    newcall = Call.create   
+    newcall.answered= params[:answered]
+    newcall.target= params[:target]
+    newcall.caller_ID = params[:callerID]
+    newcall.was_connected = params[:connected]
+    newcall.menuTime = params[:time]
     newcall.session = params[:session]
-    newcall.save
+    newcall.save  
   end
 
   
