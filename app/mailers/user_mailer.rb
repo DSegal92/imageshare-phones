0e15a84b1543f26b33497d54bc9241e3b0cb6adc
Daniel Segal <DSegal92@gmail.com>
2012-07-20 13:10:31 -0400

    testing

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ default from: "postmaster@vigilantnotifier.mailgun.org"
   		  
 
   def incomingCall(target, caller, sessionID)
-    call = Call.find_by_session(sessionID)
+    @call = Call.find_by_session(sessionID)
     @target = target
     @caller = caller
   	mail(to: 'DSegal92@gmail.com', :subject => "New Call for " + target)
