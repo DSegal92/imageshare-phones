59896bb43d25c07dafe941f46967dbb1a94730c1
Daniel Segal <DSegal92@gmail.com>
2012-07-23 12:05:20 -0400

    Made Caller ID link_to new tab

--- app/admin/calls.rb
+++ app/admin/calls.rb
@@ -49,7 +49,7 @@ ActiveAdmin.register Call do
    f.inputs do
      f.input :target
      f.input :answered, :label => "Answered By"
-     f.input :caller_ID, :label => link_to("Caller ID", '/admin/calls?&q%5Bcaller_ID_contains%5D=' + call.caller_ID)
+     f.input :caller_ID, :label => link_to("Caller ID", '/admin/calls?&q%5Bcaller_ID_contains%5D=' + call.caller_ID, :target => '_blank')
      f.input :menuTime, :label => "Call Length (s)"
      f.input :site, :as => :select, :collection => call.getSites
      f.input :location 
