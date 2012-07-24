2ad61e68e4fc286fe289f5f3b1976230e934ab8e
Daniel Segal <DSegal92@gmail.com>
2012-07-24 10:49:01 -0400

    testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ ImagesharePhones::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
-  
+  config.time_zone = 'Eastern Time (US & Canada)'
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
