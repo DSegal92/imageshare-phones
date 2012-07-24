0fa6e1d725f377379225979e62e172fdb0e753d0
Daniel Segal <DSegal92@gmail.com>
2012-07-24 13:13:45 -0400

    testing

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -51,7 +51,7 @@ class CallsController < ApplicationController
     newcall.was_connected = params[:connected]
     newcall.menuTime = params[:time]
     newcall.session = params[:session]
-    newcall.called_on = DateTime.now.to_date.to_s
+    newcall.called_on = DateTime.now.month
     newcall.save	
   end
 
