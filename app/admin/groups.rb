b56c356f2f7ac7e2bc2b38aa2e6cbf638d78fadb
Daniel Segal <DSegal92@gmail.com>
2012-08-02 12:44:27 -0400

    Standardized JSON

--- app/admin/groups.rb
+++ app/admin/groups.rb
@@ -12,7 +12,8 @@ ActiveAdmin.register Group do
       f.input :phones, :member_label => :identity, :as => :check_boxes
       f.input :start, :as => :datetime, :ignore_date => true, :hint => "Insert arbitrary values for first 3 dropdowns- Date is Ignored"
       f.input :endT, :as => :datetime, :ignore_date => true, :hint => "Insert arbitrary values for first 3 dropdowns- Date is Ignored"
-      f.input :days, :as => :check_boxes
+      f.input :days, :as => :check_boxes, :class => "test"
+
       f.input :enable
       
     end
--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -14,7 +14,7 @@ class CallsController < ApplicationController
       if Call.where(:caller_ID => params[:caller_ID]).where(:target => group.id.to_s).where(:created_at => group.callback.days.ago..Time.now).where(:was_connected => true).exists?
         call = Call.where(:caller_ID => params[:caller_ID]).where(:target => group.id.to_s).where(:created_at => group.callback.days.ago..Time.now).where(:was_connected => true).first()
         phone = Phone.find_by_id(call.answered)
-        render :json => {:name => call.target, :identity => call.answered, :number => phone.number, :callback => group.callback}
+        render :json => {:type => "Callback", :callerID => params[:caller_ID], :name => call.target, :identity => call.answered, :number => phone.number}
      
       # If multiple groups exist, check for one matching the current time
       elsif group_now && group_now.size > 0 && group.enable
@@ -23,7 +23,7 @@ class CallsController < ApplicationController
         if count >= group.phones.size
           count = 0
         end
-        render :json => {:type => "Normal", :callerID => params[:caller_ID], :name => group['identity'], :identity => group.phones[count]['identity'], :size => group.phones.size, :number => group.phones[count]['number']}
+        render :json => {:type => "Normal (Multiple Groups)", :callerID => params[:caller_ID], :name => group['identity'], :identity => group.phones[count]['identity'], :number => group.phones[count]['number']}
           group.incrCounter(group)
       # Else if only one group exists, check if it matches the current time
       elsif group && group.enable && (group.start.hour.to_f + (group.start.min)/100.to_f <= compTime) && (group.endT.hour.to_f + (group.endT.min)/100.to_f >= compTime) && group.days.exists?(:value => day)
@@ -31,17 +31,17 @@ class CallsController < ApplicationController
         if count >= group.phones.size
           count = 0
         end
-        render :json => {:type => "Normal", :name => group['identity'], :identity => group.phones[count]['identity'], :count => count, :number => group.phones[count]['number'] }
+        render :json => {:type => "Normal (Single Group)", :callerID => params[:caller_ID], :name => group['identity'], :identity => group.phones[count]['identity'], :number => group.phones[count]['number'] }
           group.incrCounter(group)
       # If no group matches time, but a phone matches extension return phone
       elsif phone
-        render :json => {:name => phone["identity"], :identity => phone["identity"], :number => phone["number"]}
+        render :json => {:type => "Phone (No Group in Time)", :callerID => params[:caller_ID], :name => phone["identity"], :identity => phone["identity"], :number => phone["number"]}
       else 
          render :json => {:number => false}
       end
     # If no group matches extension, but a phone matches
     elsif phone
-      render :json => {:name => phone["identity"], :identity => phone["identity"], :number => phone["number"]}
+      render :json => {:type => "Phone (No Group)", :callerID => params[:caller_ID], :name => phone["identity"], :identity => phone["identity"], :number => phone["number"]}
     # If no groups or phone match extension
     else
       render :json => {:number => false}
